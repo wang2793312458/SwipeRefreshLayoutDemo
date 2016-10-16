7969ea271e343c16a43a377e475c7fe91d5eebdc
yaozhengzheng <1624566366@qq.com>
2016-10-16 20:20:34 +0800

    增加一个adapter现场徐徐徐

---
+++ app/src/main/java/com/yao/feicui/swiperefreshlayoutdemo/B.java
@@ -0,0 +1,32 @@
+package com.yao.feicui.swiperefreshlayoutdemo;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 这个类其实没什么用，就是测试的
+ * Created by yaozhengzheng on 2016/10/16.
+ */
+
+public class B extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
